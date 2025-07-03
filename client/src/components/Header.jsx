0bc3f48664a0d9706b7ac6ac8eb6d8ddc3c2a248
umesh <utkrisht@gmail.com>
2025-07-03 18:39:24 +0530

    create backend and install express and run on the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,18 +3,18 @@ import { MdSearch } from 'react-icons/md'
 function Header(){
     return (
         <header className="bg-slate-200 shadow-md">
-            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+            <div className="flex justify-between items-center max-w-6xl mx-auto p-3 text-xl">
                 <Link to= '/'>
-                <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-                    <span className="text-slate-500">Yogi</span>
+                <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ">
+                    <span className="text-slate-500 text-">Yogi</span>
                     <span className="text-slate-700">Estate</span>
                 </h1>
                 </Link>
-                <form className="bg-slate-100 p-3 rounded-lg flex items-center gap-2">
-                    <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64 "/>
+                <form className="bg-slate-100 p-3 rounded-lg flex items-center ">
+                    <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-78 "/>
                     <MdSearch className="text-slate-600 text-xl cursor-pointer" />
                 </form>
-                <ul className='flex gap-4'>
+                <ul className='flex gap-6 p-4 '>
                     <Link to='/'>
                       <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
                     </Link>
