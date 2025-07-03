ce88efc8e16a6e713f30fb496c7edd4380eb9613
umesh <utkrisht@gmail.com>
2025-07-03 16:27:45 +0530

    installed react and tailwind and build first templete

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App(){
+  return (
+    <>
+       <h1 className="bg-gray-500 border-2 border-b-blue-600">this is inovate real estate project</h1>
+       <div className="border-2 border-blue-900 h-40 w-40 m-4 p-16 text-red-500">div1</div>
+    </>
+  );
+}
+
+
+export default App;
