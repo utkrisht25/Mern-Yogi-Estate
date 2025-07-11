c2e0d0fb0b636e730fc2ebcb59214d7f066a3381
umesh <utkrisht@gmail.com>
2025-07-11 11:03:36 +0530

    create sign in api route

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -16,7 +16,7 @@ function SignUp(){
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch('http://localhost:3000/api/auth/signup', {
+      const res = await fetch('api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
