4ed38bd50673c8c29f140db8d97ce089d547248a
umesh <utkrisht@gmail.com>
2025-07-24 12:38:25 +0530

    listing page complete Ui

--- client/src/Pages/updateListing.jsx
+++ client/src/Pages/updateListing.jsx
@@ -250,7 +250,9 @@ export default function UpdateListing() {
                             />
                             <div className='flex flex-col items-center'>
                                 <p>Regular price</p>
-                                <span className='text-xs'>($ / month)</span>
+                                {formData.type === 'rent' && (
+                                    <span className='text-xs'>($ / month)</span>
+                                )}
                             </div>
                         </div>
                         {formData.offer && (
@@ -267,7 +269,9 @@ export default function UpdateListing() {
                                 />
                                 <div className='flex flex-col items-center'>
                                     <p>Discounted price</p>
-                                    <span className='text-xs'>($ / month)</span>
+                                    {formData.type === 'rent' && (
+                                        <span className='text-xs'>($ / month)</span>
+                                    )}
                                 </div>
                             </div>
                         )}
