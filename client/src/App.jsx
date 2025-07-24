f0e98f582935ad039c1f91c44601452b27466b72
umesh <utkrisht@gmail.com>
2025-07-24 11:54:39 +0530

    add image slider with swiper in the listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import Home from "./Pages/Home";
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import About from "./Pages/About";
+import Listing from "./Pages/Listing";
 import Profile from "./Pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./Pages/CreateListing";
 import UpdateListing from "./Pages/updateListing";
+
 
 function App(){
   return (
@@ -21,6 +23,7 @@ function App(){
         <Route path="/sign-in" element={<SignIn />}/>
         <Route path="/sign-up" element={<SignUp />}/>
         <Route path="/about" element={<About />}/>
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route element ={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />}/>
             <Route path="/create-listing" element={<CreateListing />}/>
